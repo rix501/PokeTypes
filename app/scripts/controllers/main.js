c02a242ee3312a7aac1ec76aa77fd52c35f15617
Ricardo J. Vázquez <rix501@gmail.com>
2013-10-18 18:37:43 -0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('CodeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
